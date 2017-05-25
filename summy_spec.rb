d594130466e683f74f163d777d25ff93addbe5bf
Vincent Tsang <vstsang@gmail.com>
2017-05-25 19:24:01 +0800

    Checkpoint 17: completed

---
+++ summy_spec.rb
@@ -0,0 +1,14 @@
+RSpec.describe summy do
+
+  describe "Parse sentences into arrays" do
+    it "has the correct number of sentences" do
+      text = ""
+    end
+
+    it "identifies end of sentences correctly" do
+    end
+  end
+
+
+
+end
