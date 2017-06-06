c749b8fac081a94a28734a51e59a218a2a5a722a
Vincent Tsang <vstsang@gmail.com>
2017-06-06 19:05:37 +0800

    Assignment 20: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,13 @@
 require 'random_data'
 
-50.times do
-  Post.create!(
+20.times do
+  Question.create!(
     title:  RandomData.random_sentence,
-    body:   RandomData.random_paragraph
+    body:   RandomData.random_paragraph,
+    resolved: rand(0..1) == 1 ? true : false
   )
 end
-posts = Post.all
-
-100.times do
-  Comment.create!(
-    post:  posts.sample,
-    body:  RandomData.random_paragraph
-  )
-end
+questions = Question.all
 
 puts "Seed finished"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+puts "#{Question.count} questions created"
