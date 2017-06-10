1cb38cb5863fd54f400d95f40661a66888e89f06
Vincent Tsang <vstsang@gmail.com>
2017-06-10 11:11:27 +0800

    Remove questions table

--- db/schema.rb
+++ db/schema.rb
@@ -32,14 +32,6 @@ ActiveRecord::Schema.define(version: 20170606225051) do
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
 
-  create_table "questions", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.boolean  "resolved"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "topics", force: :cascade do |t|
     t.string   "name"
     t.boolean  "public",      default: true
