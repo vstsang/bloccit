3526622ef007b88d9d39c087457d95e1b78acd10
Vincent Tsang <vstsang@gmail.com>
2017-06-16 18:23:19 +0800

    Checkpoint 25: completed

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:new, :create]
-  
+
+  resources :sessions, only: [:new, :create, :destroy]
+
   get 'about' => 'welecome#about'
 
   root 'welcome#index'
