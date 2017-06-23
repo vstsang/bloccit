5184dee1a80f5b39dd95dcbc5ee025571c4eb3ae
Vincent Tsang <vstsang@gmail.com>
2017-06-23 18:41:32 +0800

    Checkpoint 28: completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620095713) do
+ActiveRecord::Schema.define(version: 20170623090825) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20170620095713) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "role"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -35,10 +35,19 @@ posts = Post.all
   )
 end
 
-user = User.first
-user.update_attributes!(
-  email: 'vstsang@gmail.com',
-  password: 'password'
+# Create an admin user
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+
+# Create a member
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld'
 )
 
 puts "Seed finished"
