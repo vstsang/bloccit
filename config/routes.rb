7627f8728fadeab63acfeb221157362df94c16d1
Vincent Tsang <vstsang@gmail.com>
2017-06-27 06:31:03 +0800

    Checkpoint 29: completed

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,15 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
   end
 
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
+  end
+
   resources :users, only: [:new, :create]
 
   resources :sessions, only: [:new, :create, :destroy]
 
-  get 'about' => 'welecome#about'
+  get 'about' => 'welcome#about'
 
   root 'welcome#index'
 end
