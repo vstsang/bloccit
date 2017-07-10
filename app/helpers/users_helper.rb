837e245a18a082890e8f7187e6168fe36d999b6b
Vincent Tsang <vstsang@gmail.com>
2017-07-10 12:39:14 +0800

    debug partial in users helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,20 @@
 module UsersHelper
+  def display_posts_and_comments(user)
+    if @user.posts.count.zero? && @user.comments.count.zero?
+      content_tag :h3, "#{user.name} has not submitted any posts yet."
+    else
+      content_tag :h2 do
+        concat content_tag :p, 'Posts'
+        concat content_tag :small,
+          @user.posts.each do |post|
+            render({ partial: 'posts/post', locals: {post: post} })
+          end
+        concat content_tag :p, 'Comments'
+        concat content_tag :small,
+          @user.comments.each do |comment|
+            render({ partial: 'comments/comment', locals: {comment: comment} })
+          end
+      end
+    end
+  end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Comment, type: :model do
   let(:topic) { create(:topic) }
   let(:user) { create(:user) }
   let(:post) { create(:post) }
-  let(:comment) { Comment.create!(body: 'Comment Body', post: post, user: user) }
+  let(:comment) { create(:comment) }
 
   it { is_expected.to belong_to(:post) }
   it { is_expected.to belong_to(:user) }
@@ -14,13 +14,13 @@ RSpec.describe Comment, type: :model do
 
   describe "attributes" do
     it "has a body attribute" do
-      expect(comment).to have_attributes(body: "Comment Body")
+      expect(comment).to have_attributes(body: comment.body)
     end
   end
 
   describe "after_create" do
     before do
-      @another_comment = Comment.new(body: 'Comment Body', post: post, user: user)
+      @another_comment = Comment.new(body: comment.body, post: post, user: user)
     end
 
 # #23
